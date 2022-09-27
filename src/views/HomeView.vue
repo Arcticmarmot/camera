8ba17d41bd9b56144356e85207b288368f30c0ca
Arcticmarmot <arctic_marmot@outlook.com>
2022-09-27 17:50:50 +0800

    camera rect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,12 @@
   <div id="home">
     <h3 class="title">人脸识别</h3>
     <div class="video-wrapper">
-      <div class="shade"></div>
-      <video id="video" width="300" height="300" autoPlay>
+      <div class="shade-left"></div>
+      <video id="video" width="300" height="150" autoPlay>
         <track default kind="captions" srcLang="en" src="" />
         您的浏览器不支持 video 标签。
       </video>
+      <div class="shade-right"></div>
     </div>
     <div class="tip"><strong>请眨眼</strong></div>
   </div>
@@ -51,15 +52,24 @@ export default class HomeView extends Vue {
     width: 300px;
     height: 300px;
     border-radius: 200px;
-    .shade{
-      border-radius: 200px;
+    .shade-left{
+      background: white;
       border-color: white;
       position: absolute;
-      z-index: 10;
-      width: 300px;
-      height: 300px;
+      width: 75px;
+      height: 150px;
+    }
+    .shade-right{
+      left: 225px;
+      top: 0px;
+      background: white;
+      border-color: white;
+      position: absolute;
+      width: 75px;
+      height: 150px;
     }
     #video{
+      z-index: 10;
       border-radius: 200px;
       object-fit: fill;
     }
