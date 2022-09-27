60a4bf10f3ffb335d87cf582ee4a524f9868c49a
Arcticmarmot <arctic_marmot@outlook.com>
2022-09-27 15:52:57 +0800

    docs/js complete

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: "./",
+  publicPath: "/camera",
   outputDir: "docs",
   transpileDependencies: true
 })
